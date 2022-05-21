55b3198f8e0efa4b9e60c72373efda417c7aa38c
Joseph24x7 <Joseph2471994@gmail.com>
2022-05-21 17:35:08 +0530

    Adding Rest Controller

--- src/main/java/com/account/management/controller/AccountManagementController.java
+++ src/main/java/com/account/management/controller/AccountManagementController.java
@@ -4,25 +4,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.account.management.models.Account;
 import com.account.management.models.BusinessAccount;
 import com.account.management.models.PersonalAccount;
 
+@RestController
 public class AccountManagementController {
 
 	@GetMapping("/getAccountDetails/{category}/{accountNumber}")
 	public ResponseEntity<Account> getAccountDetails(@PathVariable String category, @PathVariable String accountNumber) {
 
-		Account account = null;
 		if (category.equalsIgnoreCase("Personal")) {
-
-			account = new PersonalAccount();
-		} else {
-			account = new BusinessAccount();
+			
+			PersonalAccount personalAccount = new PersonalAccount();
+			personalAccount.setPersonalName("Personal Name");
+			return new ResponseEntity<Account>(personalAccount, HttpStatus.OK);
+		
+		} else if (category.equalsIgnoreCase("Business")) {
+			
+			BusinessAccount businessAccount = new BusinessAccount();
+			return new ResponseEntity<Account>(businessAccount, HttpStatus.OK);
+			
 		}
 
-		return new ResponseEntity<Account>(account, HttpStatus.OK);
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 
 }
